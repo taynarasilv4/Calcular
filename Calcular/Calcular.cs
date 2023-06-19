a9e09d9f3bc83e45bc08838e2e36d44186737de0
Taynara <taynarasilvarodriguesr@gmail.com>
2023-06-19 12:52:53 -0400

    primeiro

---
+++ Calcular/Calcular.cs
@@ -0,0 +1,23 @@
+﻿public class Calcular
+{
+    
+    public double Soma(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+
+    public double Subtracao(double n3, double n4)
+    {
+        return n3 - n4;
+    }
+
+    public double Multiplicacao(double n5, double n6)
+    {
+        return n5 * n6;
+    }
+
+    public double divisao(double n7, double n8)
+    {
+        return n7 / n8;
+    }
+}
---
+++ Calcular/Program.cs
@@ -0,0 +1,25 @@
+﻿    Calcular c = new Calcular();
+    double n1 = Convert.ToDouble(Console.ReadLine());
+    double n2 = Convert.ToDouble(Console.ReadLine());
+    double retornoSoma = c.Soma(n1, n2);
+    Console.WriteLine(retornoSoma);
+
+    Calcular d = new Calcular();
+    double n3 = Convert.ToDouble(Console.ReadLine());
+    double n4 = Convert.ToDouble(Console.ReadLine());
+    double retornoSubtracao = d.Subtracao(n1, n2);
+    Console.WriteLine(retornoSubtracao);
+
+    Calcular e = new Calcular();
+    double n5 = Convert.ToDouble(Console.ReadLine());
+    double n6 = Convert.ToDouble(Console.ReadLine());
+    double retornoMultiplicacao = e.Multiplicacao(n1, n2);
+    Console.WriteLine(retornoMultiplicacao);
+
+    Calcular f = new Calcular();
+    double n7 = Convert.ToDouble(Console.ReadLine());
+    double n8 = Convert.ToDouble(Console.ReadLine());
+    double retornoDivisao = f.divisao(n1, n2);
+    Console.WriteLine(retornoDivisao);
+
+    
